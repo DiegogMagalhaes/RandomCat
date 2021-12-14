caa81dad401b199db4db42f10a5ffb1ac3c1ae0c
Diego Gabriel <49524067+DiegogMagalhaes@users.noreply.github.com>
2021-12-14 16:53:52 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 import os
 from bottle import Bottle, request, template, debug,route, error, run
 import requests
+import random
 
 @route('/')
 def index():
@@ -9,21 +10,39 @@ def index():
 <h1>RandomCat</h1>
 <h2>Menu</h2>
     <a href="foto?tipo=aleatoria">Random Image </a> <br>
-    <a>Random Gif </a> <br>
+    <a href="foto?tipo=gif">Random Gif </a> <br>
     <a>Cat Saying... </a> <br>
+    
+    <form action="foto?tipo=text" method="POST">
+    <textarea name="text" rows ="3" cols="20">
+    </textarea>
+    <input value="Enviar" type="submit" />
     </form>
 '''
     
 
 @route('/foto', method='POST')
 @route('/foto', method='GET')
-def exibe_foto():
-    raca = str(request.forms.get('raca'))
+def exibe_foto():    
     tipo = str(request.params.get('tipo'))
+    text = str(request.params.get('text'))
     if "aleatoria" in tipo:
         response = requests.get('https://cataas.com/cat?json=true')
         img = response.json().get("id");
         return f" <img src='https://cataas.com/cat/{img}' alt=''><br><br><a href=\"/\">Voltar </a> <br>" 
+    
+    if "gif" in tipo:
+        response = requests.get('https://cataas.com/api/cats?tags=gif&limit=100')
+        r = random.randint(0, 100) 
+        json = response.json()
+        img =  json[r]['id']   
+        return f" <img src='https://cataas.com/cat/{img}' alt=''><br><br><a href=\"/\">Voltar </a> <br>" 
+    
+    if "text" in tipo:
+        response = requests.get('https://cataas.com/cat?json=true')
+        img = response.json().get("id");
+        return f" <img src='https://cataas.com/cat/{img}/says/{text}' alt=''><br><br><a href=\"/\">Voltar </a> <br>" 
+        
 
     
         
